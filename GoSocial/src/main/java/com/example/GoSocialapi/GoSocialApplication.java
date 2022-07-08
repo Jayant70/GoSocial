937468721d77c19487526f30c735cc623196d0ef
Jayant70 <jayanthimanshu70@gmail.com>
2022-07-08 17:03:14 +0530

    Application properties allowed methods

--- GoSocial/src/main/java/com/example/GoSocialapi/GoSocialApplication.java
+++ GoSocial/src/main/java/com/example/GoSocialapi/GoSocialApplication.java
@@ -1,3 +1,4 @@
+/*
 package com.example.GoSocialapi;
 
 import org.springframework.boot.SpringApplication;
@@ -12,5 +13,37 @@ public class GoSocialApplication {
 	}
 
 }
+
+*/
+
+package com.example.GoSocialapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SuppressWarnings("deprecation")
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class GoSocialApplication {
+   public static void main(String[] args) {
+      SpringApplication.run(GoSocialApplication.class, args);
+   }
+   @Bean
+   public WebMvcConfigurer corsConfigurer() {
+      return new WebMvcConfigurerAdapter() {
+         @Override
+         public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("")
+            .allowedHeaders("*");
+         }
+      };
+   }
+}
+
+
 
 
